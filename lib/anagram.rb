171ee6c70e7fee760d2cfd81b921e7ba3927670a
benola247 <benola247@yahoo.com>
2020-10-30 09:55:47 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :some_word
+
+    def initialize(word)
+        @some_word = word
+    end
+
+    def match(array)
+        array.select {|each_word| each_word.split("").sort == @some_word.split("").sort}
+    end
+
+end
+
